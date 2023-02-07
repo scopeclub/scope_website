da77e25f200fc066fb3d9132bba0510329fe8c8a
siddharthkanna <thesiddharthk@gmail.com>
2023-02-07 22:44:16 +0530

    team data

---
+++ src/Assets/Data/TeamData.js
@@ -0,0 +1,40 @@
+export const bod = [
+  {
+    id: 0,
+    name: "Dr.N.V.RAJASEKHAR REDDY",
+    position: "Club Head",
+    alt: "rajsekhar sir",
+    image: "sample.jpg",
+    socials: {
+      linkedin: "https://www.linkedin.com/in/rajasekharreddynv/",
+      github: '',
+      instagram:'',
+    },
+  },
+  {
+    id: 1,
+    name: "Dr.N.V.RAJASEKHAR REDDY",
+    position: "Club Head",
+    alt: "rajsekhar sir",
+    image: "sample.jpg",
+    socials: {
+      linkedin: "https://www.linkedin.com/in/rajasekharreddynv/",
+      github: '',
+      instagram:'',
+    },
+  },
+  {
+    id: 2,
+    name: "Dr.N.V.RAJASEKHAR REDDY",
+    position: "Club Head",
+    alt: "rajsekhar sir",
+    image: "sample.jpg",
+    socials: {
+      linkedin: "https://www.linkedin.com/in/rajasekharreddynv/",
+      github: '',
+      instagram:'',
+    },
+  },
+];
+
+export const TeamData2k24 = [{}];
