afcfc9f54c82a1350c05e446b3d4780bb7069076
ch-premchand <20r21a0412@mlrinstitutions.ac.in>
2023-02-16 23:27:26 +0530

    partially updated team page(social media links left)

---
+++ src/Assets/Data/TeamData25.js
@@ -0,0 +1,77 @@
+export const t25 = [
+    {
+      id: 0,
+      name: "Dr.N.V.RAJASEKHAR REDDY",
+      position: "Club Head",
+      alt: "rajsekhar sir",
+      image: "sample.jpg",
+      socials: {
+        linkedin: "https://www.linkedin.com/in/rajasekharreddynv/",
+        github: '',
+        instagram:'',
+      },
+    },
+    {
+      id: 1,
+      name: "Dr.N.V.RAJASEKHAR REDDY",
+      position: "Club Head",
+      alt: "rajsekhar sir",
+      image: "sample.jpg",
+      socials: {
+        linkedin: "https://www.linkedin.com/in/rajasekharreddynv/",
+        github: '',
+        instagram:'',
+      },
+    },
+    {
+      id: 2,
+      name: "Dr.N.V.RAJASEKHAR REDDY",
+      position: "Club Head",
+      alt: "rajsekhar sir",
+      image: "sample.jpg",
+      socials: {
+        linkedin: "https://www.linkedin.com/in/rajasekharreddynv/",
+        github: '',
+        instagram:'',
+      },
+    },
+    {
+      id: 3,
+      name: "Dr.N.V.RAJASEKHAR REDDY",
+      position: "Club Head",
+      alt: "rajsekhar sir",
+      image: "sample.jpg",
+      socials: {
+        linkedin: "https://www.linkedin.com/in/rajasekharreddynv/",
+        github: '',
+        instagram:'',
+      },
+    },
+    {
+      id: 4,
+      name: "Dr.N.V.RAJASEKHAR REDDY",
+      position: "Club Head",
+      alt: "rajsekhar sir",
+      image: "sample.jpg",
+      socials: {
+        linkedin: "https://www.linkedin.com/in/rajasekharreddynv/",
+        github: '',
+        instagram:'',
+      },
+    },
+    {
+      id: 5,
+      name: "Dr.N.V.RAJASEKHAR REDDY",
+      position: "Club Head",
+      alt: "rajsekhar sir",
+      image: "sample.jpg",
+      socials: {
+        linkedin: "https://www.linkedin.com/in/rajasekharreddynv/",
+        github: '',
+        instagram:'',
+      },
+    },
+  ];
+  
+  
+  
