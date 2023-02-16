56819d9c1162ecfeaf3a05f4ee226a5a81707d25
siddharthkanna <thesiddharthk@gmail.com>
2023-02-16 13:02:14 +0530

    hosting

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,21 @@
 import "./Home.css";
+import { motion } from "framer-motion";
+import ParticleBackground from "../ParticleBackground";
 
 function Home() {
   return (
-    <div>
-      <div className="home-container">We are Scope Club</div>
-    </div>
+    <motion.div
+      className="home-container"
+      initial={{ y: 1, scale: 1, opacity: 0 }}
+      animate={{
+        y: "20px",
+        opacity: 1,
+      }}
+      transition={{ duration: 0.5 }}
+    >
+      <ParticleBackground />
+      <h1 className="home-h">We are Scope Club.</h1>
+    </motion.div>
   );
 }
 
