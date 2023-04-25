578127c3c621b893b55fa18507d614011d28bff5
siddharthkanna <thesiddharthk@gmail.com>
2023-04-26 00:29:20 +0530

    Appfiesta_3

--- src/components/AppFiesta.js
+++ src/components/AppFiesta.js
@@ -1,35 +1,38 @@
 import React from "react";
 import "./AppFiesta.css";
-import Qr from '../Assets/qrcode.jpg'
-import AppLogo from '../Assets/appfiesta_logo.png'
+import Qr from "../Assets/qrcode.jpg";
+
 function AppFiesta() {
   return (
     <div className="AppFiesta">
-        
       <div className="event">
-
-        <img src={AppLogo} id="alogo" />
-        <div></div>
-        <p>2 day Bootcamp + 1 day Hackathon</p>
-        <img src={Qr} />
+        <h1>APPFIESTA</h1>
+        <img src={Qr} alt="qr" id="qr" />
         <p>UPI Phone Number: 9603166060</p>
-        <p>Scan here to get ready with your team and take part in APPFIESTA!!</p>
-        <h4>Team size - Amount</h4>
-        <h5>  3       - Rs.1110/-</h5>
-        <h5>  4       - Rs.1480/-</h5>
-        <p>Note:PLease make sure to note the Transaction ID for filling the form.</p>
-
+        <div className="eve-content">
+          <h2>
+            Scan here to get ready with your team
+            <br /> and take part in APPFIESTA!
+          </h2>
+          <h4>Team size - Amount</h4>
+          <h4> 3 - Rs.1110/-</h4>
+          <h4> 4 - Rs.1480/-</h4>
+          <h3>
+            Note: Please make sure to note
+            <br /> the Transaction ID for filling the form.
+          </h3>
+        </div>
       </div>
-      <iframe
-        title="AppFiesta"
-        src="https://lu.ma/embed-checkout/evt-eZEXm3GQlkBopdT"
-        width="1000"
-        height="500"
-        frameborder="0"
-        allowfullscreen=""
-        aria-hidden="false"
-        tabindex="0"
-      ></iframe>
+      <div className="iframe-container">
+        <iframe
+          title="AppFiesta"
+          src="https://lu.ma/embed-checkout/evt-S6fBq7jBakEELUC"
+          frameborder="0"
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+      </div>
     </div>
   );
 }
