360f91f4f1490a1eab002a740126913db83d1809
sedulousprem <20r21a0412@mlrinstitutions.ac.in>
2023-04-26 14:25:02 +0530

    prize update

--- src/components/AppFiesta.js
+++ src/components/AppFiesta.js
@@ -58,7 +58,7 @@ function AppFiesta() {
           </h2>
           <h2>
             Register now and stand a chance 
-            <br /> to win prize money up to Rs.15000/-
+            <br /> to win prize money up to <b>Rs.15000/-</b>
           </h2>
           <br/>
           <h3>
